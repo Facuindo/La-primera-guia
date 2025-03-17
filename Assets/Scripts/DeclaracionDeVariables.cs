5ec41d12929d2ad47d034bf8b7f74205d616b4e3
Facuindo <167240285+Facuindo@users.noreply.github.com>
2025-03-17 12:14:11 -0300

    Variables Declaradas y Inicializadas

---
+++ Assets/Scripts/DeclaracionDeVariables.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeclaracionDeVariables : MonoBehaviour
+{
+    int cantidadAlumnos;
+    float largoMesaenM;
+    string domiciloUsuario;
+    bool aceptaCondiciones;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cantidadAlumnos = 28;
+        largoMesaenM = 1.70f;
+        domiciloUsuario = "Libertador 5670";
+        aceptaCondiciones = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
